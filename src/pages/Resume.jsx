c73e91c9d6976f8bfe4699d72371293144d375b5
Giancarlo Whitaker <giancarlo.whitaker@gmail.com>
2023-11-06 11:38:29 -0800

    Add Resume section content

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -3,7 +3,29 @@ export default function Resume() {
       <>
         <h1 className="section-header">RESUME</h1>
         <section className="section-contents">
+            <p><a href="../public/docs/Giancarlo_Whitaker_Resume.docx">Download my resume</a>!</p>
+            <h2>Proficiencies</h2>
+            
+            <h3>Front-end proficiencies</h3>
+            <ul>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>JavaScript</li>
+                <li>jQuery</li>
+                <li>React</li>
+                <li>Tailwind</li>
+                <li>Responsive Design</li>
+            </ul>
 
+            <h3>Back-end proficiencies</h3>
+            <ul>
+                <li>SQL, MySQL, and Sequelize</li>
+                <li>Node</li>
+                <li>Express</li>
+                <li>MongoDB and Mongoose</li>
+                <li>REST</li>
+                <li>APIs</li>
+            </ul>
         </section>
       </>
     );
