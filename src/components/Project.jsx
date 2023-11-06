b61fd53d6b9421d567797ada1efa34d2451c94b8
Giancarlo Whitaker <giancarlo.whitaker@gmail.com>
2023-11-05 21:15:56 -0800

    Change Project to loop over a set of props

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,5 +1,5 @@
-export default function Project () {
-    return (
+export default function Project (props) {
+    /*return (
         <div className="card">
             <img src=""/>
             <div className="card-text">
@@ -7,5 +7,18 @@ export default function Project () {
                 <p>Project text</p>
             </div>
         </div>
+    );*/
+    return (
+        <ul className="section-contents project-grid">
+            {props.data.map( (item) => (
+                <li className="card">
+                    <img src={item.imgurl}/>
+                    <div className="card-text">
+                        <h3>{item.name}</h3>
+                        <p>Project text</p>
+                    </div>
+                </li>
+            ))}
+        </ul>
     );
 }
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -4,15 +4,7 @@ export default function Portfolio() {
     return (
       <>
         <h1 className="section-header">PORTFOLIO</h1>
-        <section className="section-contents project-grid">
-            <Project />
-            <Project />
-            <Project />
-            <Project />
-            <Project />
-            <Project />
-        </section>
+        <Project projectData={data}/>
       </>
     );
-  }
-  
+}
