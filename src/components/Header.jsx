907aa2b6c8bce6ade7402ef461d86b0d0768ae89
Giancarlo Whitaker <giancarlo.whitaker@gmail.com>
2023-11-05 18:31:06 -0800

    Add (mostly blank) Header and Navigation components

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+
+
+function Header () {
+    return (
+        <header>
+            <h1>Giancarlo Whitaker</h1>
+            <nav><Navigation /></nav>
+        </header>
+    );
+}
+
+export default Header;
