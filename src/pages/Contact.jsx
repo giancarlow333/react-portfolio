ebb3bd442e8ea23a458a55d07ed3e45c30e5ee95
Giancarlo Whitaker <giancarlo.whitaker@gmail.com>
2023-11-06 11:28:30 -0800

    Add mostly empty <Contact /> and <Resume /> components

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,12 @@
+export default function Contact() {
+    return (
+      <>
+        <h1 className="section-header">CONTACT</h1>
+        <section className="section-contents">
+            <p><strong>CONTACT FORM IS NOT YET IMPLEMENTED</strong></p>
+            <p>Please contact me at: [my first name] dot [my last name] at [a mail service brought to you by the letter G] dot [com]</p>
+        </section>
+      </>
+    );
+  }
+  
---
+++ src/pages/Resume.jsx
@@ -0,0 +1,11 @@
+export default function Resume() {
+    return (
+      <>
+        <h1 className="section-header">RESUME</h1>
+        <section className="section-contents">
+
+        </section>
+      </>
+    );
+  }
+  
