432d5cad3d26f2c82fc5fbb732a9f2753335e6dd
Giancarlo Whitaker <giancarlo.whitaker@gmail.com>
2023-11-06 13:13:07 -0800

    Update Form.jsx to display error message when you tab out:

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -7,6 +7,10 @@ function validateEmail(email) {
     return re.test(String(email).toLowerCase());
 }
 
+function alertFieldRequired() {
+  return alert("This field is required!");
+}
+
 function Form() {
   // Create state variables for the fields in the form
   // We are also setting their initial values to an empty string
@@ -37,15 +41,15 @@ function Form() {
     // Preventing the default behavior of the form submit (which is to refresh the page)
     e.preventDefault();
 
-    // First we check to see if the email is not valid or if the userName is empty. If so we set an error message to be displayed on the page.
+    // First we check to see if the username is blank.
+    if (!userName) {
+      setErrorMessage("Name cannot be blank");
+      return;
+    }
+    // Then we check to see if the email is not valid
     if (!validateEmail(email)) {
       setErrorMessage('Email is invalid');
       // We want to exit out of this code block if something is wrong so that the user can correct it
-      return;
-    }
-    // Then we check to see if the username is blank.
-    if (!userName) {
-      setErrorMessage("Name cannot be blank");
       return;
     }
     // Then we check to see if the message is blank.
@@ -69,6 +73,7 @@ function Form() {
           value={userName}
           name="userName"
           onChange={handleInputChange}
+          onBlur={handleFormSubmit}
           type="text"
           placeholder="Name"
         />
@@ -76,6 +81,7 @@ function Form() {
           value={email}
           name="email"
           onChange={handleInputChange}
+          onBlur={handleFormSubmit}
           type="email"
           placeholder="Email"
         />
@@ -83,6 +89,7 @@ function Form() {
           value={message}
           name="message"
           onChange={handleInputChange}
+          onBlur={handleFormSubmit}
           type="text"
           placeholder="Message"
         />
