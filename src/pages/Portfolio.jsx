5e905d553a3eed3b7f4efb3b92bfb9deff148ea4
Giancarlo Whitaker <giancarlo.whitaker@gmail.com>
2023-11-05 22:11:00 -0800

    Add projects to portfolio, with links and images

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -6,11 +6,46 @@ export default function Portfolio() {
         <h1 className="section-header">PORTFOLIO</h1>
         <section className="section-contents project-grid">
           <Project 
-            imgurl="https://giancarlow333.github.io/portfolio/assets/img/solar_system_diagrammer.png"
+            imgurl="/img/solar_system_diagrammer.png"
             name="Solar System Diagram Generator"
             text="JavaScript/SVG"
             github="https://github.com/giancarlow333/solar-system-diagrammer"
             livelink="https://giancarlow333.github.io/solar-system-diagrammer/"
+          />
+          <Project 
+            imgurl="/img/ravishing_reads.png"
+            name="Ravishing Reads"
+            text="Handlebars/MySQL"
+            github="https://github.com/giancarlow333/ravishing-reader"
+            livelink="http://ravishing-reads-a2209ea97ad8.herokuapp.com/"
+          />
+          <Project 
+            imgurl="/img/property-mgmt-portal.png"
+            name="Property Portal"
+            text="JavaScript/jQuery/Tailwind/APIs"
+            github="https://github.com/giancarlow333/property-mgmt-portal"
+            livelink="https://giancarlow333.github.io/property-mgmt-portal/index.html"
+          />
+          <Project 
+            imgurl="/img/coding-quiz_mockup.png"
+            name="Coding Quiz"
+            text="JavaScript/CSS"
+            github="https://github.com/giancarlow333/coding-quiz"
+            livelink="https://giancarlow333.github.io/coding-quiz/"
+          />
+          <Project 
+            imgurl="/img/note_taker.png"
+            name="JS Note Taker"
+            text="JavaScript/CSS"
+            github="https://github.com/giancarlow333/note-taker"
+            livelink="https://limitless-bastion-53747-30bc20a6077a.herokuapp.com/notes"
+          />
+          <Project 
+            imgurl="/img/sql_cms_in_use.png"
+            name="SQL CMS"
+            text="MySQL/Node.js Inquirer"
+            github="https://github.com/giancarlow333/sql-cms"
+            livelink="NULL"
           />
         </section>
       </>
