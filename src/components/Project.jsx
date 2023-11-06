b319a9c7b574711a6d6abe1b45c7386ae5db0119
Giancarlo Whitaker <giancarlo.whitaker@gmail.com>
2023-11-05 21:29:22 -0800

    Further attempts

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,13 +1,4 @@
 export default function Project (props) {
-    /*return (
-        <div className="card">
-            <img src=""/>
-            <div className="card-text">
-                <h3>Project Title</h3>
-                <p>Project text</p>
-            </div>
-        </div>
-    );*/
     return (
         <ul className="section-contents project-grid">
             {props.data.map( (item) => (
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,10 +1,21 @@
 import Project from "../components/Project";
 
 export default function Portfolio() {
+  const projdata = [
+    {
+      imgurl: "../assets/pexels-instawalli-169789.jpg",
+      name: "Project1"
+    },
+    {
+      imgurl: "../assets/pexels-instawalli-169789.jpg",
+      name: "Project2"
+    },
+  ];
+
     return (
       <>
         <h1 className="section-header">PORTFOLIO</h1>
-        <Project projectData={data}/>
+        <Project data={{projdata}}/>
       </>
     );
 }
